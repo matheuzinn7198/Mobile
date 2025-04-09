d6e1d9da8c1c8a49b0ade7b293f295d2144ea053
matheuzinn7198 <craquedebola07@gmail.com>
2025-04-09 16:54:10 -0300

    09.04.25

--- IntroducaoFlutter/exemplo_widget_interacao/lib/main.dart
+++ IntroducaoFlutter/exemplo_widget_interacao/lib/main.dart
@@ -20,6 +20,7 @@ class _TelaCadastroAppState extends State<TelaCadastroApp>{
   String _dataNascimento = "";
   double _experiencia = 0;
   bool _aceite = false; //declaração da boolean (bool)
+  bool _senhaOculta = true;
 
   //métodos
   @override
@@ -38,35 +39,47 @@ class _TelaCadastroAppState extends State<TelaCadastroApp>{
                 validator: (value) => value!.trim().isEmpty ? "Digite um Nome" : null, //operador ternário
                 onSaved: (value)=> _nome = value!.trim(),
               ),
+              SizedBox(height: 15,),
               //Campo Email
               TextFormField(
                 decoration: InputDecoration(labelText: "Insira o Email"),
                 validator: (value) => value!.contains("@") ? null : "Digite um Email Válido",
                 onSaved: (value) => _email = value!.trim(),
               ),
+              SizedBox(height: 15,),
               //Campo Senha
               TextFormField(
-                decoration: InputDecoration(labelText: "Insira a Senha"),
-                obscureText: true,
+                decoration: InputDecoration(labelText: "Insira a Senha",
+                prefixIcon: IconButton(
+                  onPressed: (){
+                    setState(() {
+                      _senhaOculta = !_senhaOculta;
+                    });
+                  }, 
+                  icon: Icon(Icons.lock))),
+                obscureText: _senhaOculta,
                 validator: (value) => value!.trim().length>=6 ? null : "Digite uma Senha Válida",
                 onSaved: (value)=>_senha = value!.trim(),
               ),
+              SizedBox(height: 15,),
               //Campo Gênero
-              Text("Genêro"),
+              Text("Gênero"),
               DropdownButtonFormField(
                 items: ["Feminino", "Masculino", "Outro"].map((String genero)=>DropdownMenuItem(
                   value: genero,
-                  child: Text(genero),)).toList(), 
-                onChanged: (value) {},
-                validator: (value) => value==null ? "Selecione um Genero" : null,
+                  child:Text(genero))).toList(), 
+                onChanged: (value){},
+                validator: (value) => value==null ? "Selecione um Gênero" : null,
                 onSaved: (value) => _genero = value!
               ),
-              //Campo Data de Nascimento
+              SizedBox(height: 15,),
+              //Campo DataNascimento
               TextFormField(
-                decoration: InputDecoration(labelText: "Insira a Data de Nascimento"),
-                validator: (value) => value!.trim().isEmpty ? "Digite uma Data de Nascimento" : null,
-                onSaved: (value) => _dataNascimento = value!.trim(),
+                decoration: InputDecoration(labelText: "Informe a Data de Nascimento"),
+                validator: (value) => value!.trim().isEmpty ? "Digite a Data de Nascimento" : null,
+                onSaved: (value)=> _dataNascimento = value!.trim(),
               ),
+              SizedBox(height: 15,),
               //slider de Seleção(experiência)
               Text("Anos de Experiência com Programação:"),
               Slider(
@@ -74,29 +87,45 @@ class _TelaCadastroAppState extends State<TelaCadastroApp>{
                 min: 0,
                 max: 20,
                 divisions: 20,
-                label: _experiencia.round().toString(),
+                label: _experiencia.round().toString(), 
                 onChanged: (value)=>setState(() {
                   _experiencia = value;
                 })),
-                //aceite dos termos de uso
+                SizedBox(height: 15,),
+                //aceite dos Termos de uso
                 CheckboxListTile(
                   value: _aceite,
-                  title: Text("Aceito os Termos de Uso do Aplicativo"), 
+                  title: Text("Aceito os Termos de Uso do Aplicativo"),
                   onChanged: (value)=>setState(() {
                     _aceite = value!;
                   })),
-                  //botão de envio do formulário
-                  ElevatedButton(
-                    onPressed: _eviarFormulario, 
-                    child: Text("Enviar")),
+                  SizedBox(height: 15,),
+                // botão de Envio do Formulário
+                ElevatedButton(
+                  onPressed: _enviarFormulario, 
+                  child: Text("Enviar"))
             ],
           )),
         ),
     );
   }
 
-  void _eviarFormulario(){
-
+  void _enviarFormulario(){
+    if(_formKey.currentState!.validate() && _aceite){
+      _formKey.currentState!.save();
+      showDialog(
+        context: context, 
+        builder: (context)=>AlertDialog(
+          title: Text("Dados do Formulário"),
+          content: Column(
+            children: [
+              Text("Nome: $_nome"),
+              Text("Email: $_email")
+              //outras informações
+            ],
+          ),
+        ));
+    }
   }
 
 }
