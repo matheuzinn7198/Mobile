f604d481a2aaf0ca23fe4b349bbaecb1f9eeb49f
matheuzinn7198 <craquedebola07@gmail.com>
2025-05-07 16:54:26 -0300

    07.05.25

---
+++ PersistenciaDadosFlutter/SharedPreferences/exemplo2_shared_preferences/lib/tela_cadastro.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+class TelaCadastro extends StatelessWidget{
+  //atributos
+  TextEditingController _nomeController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+
+  //métodos
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text("Tela de Cadastro"),),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TextField(
+            controller: _nomeController,
+            decoration: InputDecoration(labelText: "Nome"),
+          ),
+          TextField(
+            controller: _nomeController,
+            decoration: InputDecoration(labelText: "Email"),
+          ),
+          SizedBox(height: 20,),
+          ElevatedButton(
+          onPressed: _cadastrarUsuario(context),
+          child: Text("Cadastrar"),
+          ),
+        ],
+        ),
+        ), 
+      );
+  }
+
+
+_cadastrarUsuario(BuildContext context) async{
+  String _nome = _nomeController.text.trim();
+  String _email = _emailController.text.trim();
+  if(_nome.isEmpty || _email.isEmpty){
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Preencha todos os Campos!")));
+    }else{
+      SharedPreferences _prefs = await SharedPreferences.getInstance();
+      _prefs.setString(_nome, _email); //salva na chave nome o email
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Usuario Cadastrado com sucesso!")));
+    _nomeController.clear();
+    _emailController.clear();
+    Navigator.pushNamed(context, "/");
+    }
+  }
+}
