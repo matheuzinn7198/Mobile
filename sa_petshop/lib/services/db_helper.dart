42b9b478586d7428c5bc771b7df4d71ab62a8192
matheuzinn7198 <craquedebola07@gmail.com>
2025-06-04 16:52:31 -0300

    04.06.25

--- sa_petshop/lib/services/db_helper.dart
+++ sa_petshop/lib/services/db_helper.dart
@@ -1,7 +1,13 @@
 //classe de apoio as conexões do banco de dados
 // classe Singleton -> de objeto único
 
+
+
+
+import 'dart:math';
+
 import 'package:path/path.dart';
+import 'package:sa_petshop/models/consulta_model.dart';
 import 'package:sa_petshop/models/pet_model.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite/sqlite_api.dart';
@@ -48,7 +54,7 @@ class DbHelper {
       telefone TEXT NOT NULL
       )"""
     );
-    print("Tabela Pet Criada")
+    print("Tabela Pet Criada");
     //criar a tabela das consultas
     await db.execute(
       """CREATE TABLE IF NOT EXISTS consultas(
@@ -71,7 +77,7 @@ class DbHelper {
 
   Future<List<Pet>> getPets() async{
     final db = await database;
-    final List<Map<String,dynamic>> map = await db.query("pets"); //receber as info do BD
+    final List<Map<String,dynamic>> maps = await db.query("pets"); //receber as info do BD
     //converter os valores para obj
     return maps.map((e)=> Pet.fromMap(e)).toList();
   }
@@ -94,6 +100,30 @@ class DbHelper {
   }
 
   //métodos CRUD para Consultas
+  //Create Consulta
+  Future<int> insertConsulta(Consulta consulta) async{
+    final db  = await database;
+    return await db.insert("consultas", consulta.toMap());
+  }
+
+  //Get Consulta -> By Pet
+  Future<List<Consulta>> getConsultaByPetId(int petId) async{
+    final db = await database;
+    final List<Map<String,dynamic>> maps = await db.query(
+      "xonsultas",
+      where: "pet_id = ?",
+      whereArgs: [petId],
+      orderBy: "data_hora ASC" //ordenar por data e hora da consulta
+    ); //select from consultas where pet_id = ?, Pet_id, order by data_hora ASC
+    //converter a map em obj
+    return maps.map((e)=>Consulta.fromMap(e)).toList();
+  }
+
+  //Delete Consulta
+  Future<int> deleteConsulta(int id) async{
+    final db = await database;
+    return await db.delete("consultas", where: "id=?", whereArgs: [id]);
+  }
   
 
 }
--- sa_petshop/lib/views/cadastro_pet_screen.dart
+++ sa_petshop/lib/views/cadastro_pet_screen.dart
@@ -14,11 +14,12 @@ class CadastroPetScreen extends StatefulWidget {
 class _CadastroPetScreenState extends State<CadastroPetScreen>{
   final _formKey = GlobalKey<FormState>(); //chave para o formulário
   final _petsController = PetController();
-
-  String _nome = "";
-  String _raca = "";
-  String _nomeDono = "";
-  String _telefone = "";
+  
+  //late
+  late String _nome;
+  late String _raca;
+  late String _nomeDono;
+  late String _telefone;
 
   _salvarPet() async{
     if (_formKey.currentState!.validate()){
@@ -36,7 +37,36 @@ class _CadastroPetScreenState extends State<CadastroPetScreen>{
   //buildar a Screen
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    
+    return Scaffold(
+      appBar: AppBar(title: Text("Cadastro de Pet"),),
+      body: Padding(padding: EdgeInsets.all(16),
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(labelText: "Nome do Pet"),
+              validator: (value) => value!.isEmpty ? "Campo não Preenchido" : null,
+              onSaved: (newValue) => _nome = newValue!,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Raça do Pet"),
+              validator: (value) => value!.isEmpty ? "Campo não Preenchido" : null,
+              onSaved: (newValue) => _raca = newValue!,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Dono do Pet"),
+              validator: (value) => value!.isEmpty ? "Campo não Preenchido" : null,
+              onSaved: (newValue) => _nomeDono = newValue!,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Telefone"),
+              validator: (value) => value!.isEmpty ? "Campo não Preenchido" : null,
+              onSaved: (newValue) => _telefone = newValue!,
+            ),
+            ElevatedButton(onPressed: _salvarPet, child: Text("Cadastrar"))
+          ],
+        )),),
+    );
   }
 }
