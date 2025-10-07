dcd7e8fa756be4f58af0ef282d2e85f630d90a7c
matheuzinn7189 <craquedebola07@gmail.com>
2025-10-07 11:25:50 -0300

    07.10.25

--- Hardware/exemplo_geolocator/lib/main.dart
+++ Hardware/exemplo_geolocator/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:exemplo_geolocator/clima_service.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
@@ -7,6 +8,7 @@ void main(){
   ));
 }
 
+
 class LocationScreen extends StatefulWidget {
   const LocationScreen({super.key});
 
@@ -15,31 +17,89 @@ class LocationScreen extends StatefulWidget {
 }
 
 class _LocationScreenState extends State<LocationScreen> {
-  //atributos
-  String locationMessage = "Localização não obtida";
 
-  void getLocation() async {
-    //lógica para obter a localização
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high
-    );
-    setState(() {
-      locationMessage = "Latitude: ${position.latitude}, Longitude: ${position.longitude}";
-    });
+String locationMessage = "";
+
+//pegar aLocalização do Dispositivo
+void _getLocation() async{
+  //verificar se o serviço de GPS está habilitado
+  bool serviceEnable = await Geolocator.isLocationServiceEnabled();
+  if(!serviceEnable){
+    locationMessage = "Serviço de Localização está Desativado";
+    return;
   }
+
+  //verificar a permissão do GPS
+  LocationPermission permission = await Geolocator.checkPermission();
+  if(permission == LocationPermission.denied){
+    //solicito a liberação do uso do GPS
+    permission = await Geolocator.requestPermission();
+    if(permission == LocationPermission.denied){
+      locationMessage = "Permissão de Localização Negada";
+      return;
+    }
+  }
+  //se a permissão foi liberada
+  Position position = await Geolocator.getCurrentPosition();
+  locationMessage = "Localização Atual: Latitude - ${position.latitude}"
+                    + ", Longitude - ${position.longitude}";
+}
+
+void _getClima() async{
+  //verificar se o serviço de GPS está habilitado
+  bool serviceEnable = await Geolocator.isLocationServiceEnabled();
+  if(!serviceEnable){
+    locationMessage = "Serviço de Localização está Desativado";
+    return;
+  }
+
+  //verificar a permissão do GPS
+  LocationPermission permission = await Geolocator.checkPermission();
+  if(permission == LocationPermission.denied){
+    //solicito a liberação do uso do GPS
+    permission = await Geolocator.requestPermission();
+    if(permission == LocationPermission.denied){
+      locationMessage = "Permissão de Localização Negada";
+      return;
+    }
+  }
+  //se a permissão foi liberada
+  Position position = await Geolocator.getCurrentPosition();
+
+  //mostrar o clima
+  try {
+    final city = await ClimaService.getCityWeatherbyPosition(position);
+    locationMessage = "${city["name"]} -- ${city["main"]["temp"] - 273}°C";
+  } catch (e) {
+    locationMessage = e.toString();
+  }
+
+}
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("GPS - Localização"),),
+      appBar: AppBar(title: Text("GPS- Location"),),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(locationMessage),
             ElevatedButton(
-              onPressed:() async{getLocation;},
-              child: Text("Obter Localização"))
+              onPressed: () async{
+                  _getLocation();
+                setState(() {
+                });
+              }, 
+              child: Text("Pegar Localização")),
+              ElevatedButton(
+                onPressed: () async{
+                  _getClima();
+                  setState(() {
+                    
+                  });
+                }, child: Text("Pegar o Clima"))
           ],
         ),
       ),
